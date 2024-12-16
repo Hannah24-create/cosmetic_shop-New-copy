f1751144223b34e20470cb0e7b626bf75d0a3561
wisdomdzontoh <wisdomdzontoh@gmail.com>
2024-12-16 23:04:46 +0000

    updates to header index.html

--- js/script.js
+++ js/script.js
@@ -31,3 +31,13 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+
+
+    // Mobile Menu Toggle
+    const menuButton = document.getElementById('mobile-menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    menuButton.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden');
+    });
